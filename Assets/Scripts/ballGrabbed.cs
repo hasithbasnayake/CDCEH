c62d426ec8735049800433b48ca5f35a0a5fe2f3
Christian Van Stralen <cvanstralen@ucsb.edu>
2024-05-28 16:55:53 -0700

    update objects to throw and code that places them back in starting position if they go out of bounds. start to add code/objects for UI

--- Assets/Scripts/ballGrabbed.cs
+++ Assets/Scripts/ballGrabbed.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class ballGrabbed : MonoBehaviour
@@ -12,19 +13,24 @@ public class ballGrabbed : MonoBehaviour
     public static event Released OnReleased;
     private bool grabbed = false;
     private Vector3 initialPosition;
+    private Quaternion initialRotation;
     // Start is called before the first frame update
     void Start()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
         initialPosition = this.gameObject.transform.position;
+        initialRotation = this.gameObject.transform.rotation;
         // Time.timeScale = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.gameObject.transform.position.y < -3)
+        if (this.gameObject.transform.position.y < -3 || this.gameObject.transform.position.y > 10 || this.gameObject.transform.position.x < -4 || this.gameObject.transform.position.x > 6 || this.gameObject.transform.position.z < -1 || this.gameObject.transform.position.z > 8)
         {
+            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            this.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            this.gameObject.transform.rotation = initialRotation;
             this.gameObject.transform.position = initialPosition;
         }
         if (grabInteractable.isSelected)
