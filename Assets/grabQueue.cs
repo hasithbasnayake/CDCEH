70a0e216582abd85df622d084295c91a415a505a
Ethan Epp <eepp@ucsb.edu>
2024-05-29 14:17:08 -0700

    Fixed Grab Sound Getting Destroyed Bug
    Uses grab interactable toolkit

--- Assets/grabQueue.cs
+++ Assets/grabQueue.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit; // Import the XR Interaction Toolkit namespace
 
 public class grabQueue : MonoBehaviour
 {
     public AudioSource grabSound;   // Sound to play when the object is grabbed
     public AudioSource impactSound; // Sound to play when the object hits the ground
+    private XRGrabInteractable grabInteractable; // Reference to the XRGrabInteractable component
 
-    private void OnTriggerEnter(Collider other)
+    void Awake()
     {
-        if (other.gameObject.tag == "Player")
+        // Get the XRGrabInteractable component
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        if (grabInteractable == null)
         {
-            grabSound.Play();
-            Destroy(gameObject);
+            // Optionally add the XRGrabInteractable component if it's not already attached
+            grabInteractable = gameObject.AddComponent<XRGrabInteractable>();
         }
+
+        // Subscribe to the onSelectEntered event which is triggered when the object is grabbed
+        grabInteractable.onSelectEntered.AddListener(PlayGrabSound);
     }
 
+    private void PlayGrabSound(XRBaseInteractor interactor)
+    {
+        // Play the grab sound
+        grabSound.Play();
+    }
+
+    void OnDestroy()
+    {
+        // Unsubscribe to prevent memory leaks
+        grabInteractable.onSelectEntered.RemoveListener(PlayGrabSound);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         // Check if the object hits the ground
